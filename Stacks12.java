fe4f1215593b0e3eeef761d4d71d89adc78d8150
Vikram Ramesh <vicky1988@gmail.com>
2020-09-07 17:03:46 +0530

    First Commit

---
+++ Stacks12.java
@@ -0,0 +1,36 @@
+//Sort Ascending Stack
+
+package com.company;
+
+import java.util.*;
+
+public class Stacks12{
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n-- > 0)
+            stack.push(s.nextInt());
+        System.out.println(stack);
+        sort(stack);
+    }
+
+    // Method to sort the elements of the stack in ascending order
+    static void sort(Stack<Integer> stack) {
+        // Write your code here
+        Stack<Integer> tmpStack = new Stack<Integer>();
+        while(!stack.isEmpty())
+        {
+            int tmp = stack.pop();
+            while(!tmpStack.isEmpty() && tmpStack.peek() > tmp)
+            {
+                stack.push(tmpStack.pop());
+            }
+            tmpStack.push(tmp);
+
+        }
+
+        System.out.println(tmpStack);
+
+        }
+}
